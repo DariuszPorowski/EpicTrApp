442a28dc44941ad9879d9a57e7f93351f370a125
Tomasz Wisniewski <tomasz.wisniewski@windowslive.com>
2016-07-30 00:34:33 +0200

    Initial frame for the service fabric

---
+++ source/TRService/Controllers/SessionsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TRService.Controllers
+{
+    public class SessionsController: ApiController
+    {
+        // GET api/sessions
+        public IEnumerable<string> Get()
+        {
+            var tempList = new List<String>();
+            tempList.Add("TempItem");
+
+            return tempList;
+        }
+    }
+}
